92e1ffa4c3d58c14e7574ea1815a3829e975ed37
SmellyCat <smellycat2014@foxmail.com>
2017-10-09 11:23:48 +0800

    添加 access logging

--- my_logger.py
+++ my_logger.py
@@ -39,3 +39,16 @@ def online_logging(log_file_name):
     rthandler.setFormatter(formatter)
     logger.addHandler(rthandler)
 
+
+def access_logging(log_file_name):
+    """Init for logging"""
+    path = os.path.split(log_file_name)
+    if not os.path.isdir(path[0]):
+        os.makedirs(path[0])
+
+    access_logger = logging.getLogger('access')
+
+    rthandler = logging.handlers.RotatingFileHandler(
+        log_file_name, maxBytes=100 * 1024 * 1024, backupCount=10)
+    access_logger.setLevel(logging.INFO)
+    access_logger.addHandler(rthandler)
